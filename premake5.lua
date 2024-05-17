284dbb4ce20081a34949df2e19981c91ab6eaeab
Gary <qiuhuidi31@Gmail.com>
2024-05-17 17:25:43 +0800

    use premake to build the whole projects

---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "Pika"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Pika
+project "Pika"
+	location "Pika"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include",
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PK_PLATFORM_WINDOWS",
+			"PK_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "PIKA_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PIKA_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PIKA_DIST"
+		optimize "On"
+
+-- Sandbox
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Pika/vendor/spdlog/include",
+		"Pika/src"
+	}
+
+	links
+	{
+		"Pika"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"PK_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PIKA_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PIKA_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PIKA_DIST"
+		optimize "On"
+	
